1cc27afb88e97496fc6f14fa04215da24767b105
Jason Lamb <jlamb@lambcorp.com>
2019-10-07 09:50:58 -0400

    Update ChkGUAPholdingsv3.sh

--- ChkGUAPholdingsv3.sh
+++ ChkGUAPholdingsv3.sh
@@ -120,11 +120,9 @@ BlockHeight=$(printf '%14s' $BlockHeight)
 echo "Percentage of total GUAP Money Supply           : $Perc%"
 echo ""
 echo "Total number of GUAP masternodes                : $MNCount"
-MNCount= $(python -c 'import os; print "{0:>14,.0f}".format(float(os.environ["MNCount"]))')
-n= $(python -c 'import os; print "{0:>14,.0f}".format(float(os.environ["n"]))')
+MNCount=$(python -c 'import os; print "{0:>14,.0f}".format(float(os.environ["MNCount"]))')
+n=$(python -c 'import os; print "{0:>14,.0f}".format(float(os.environ["n"]))')
 echo ""
-echo "n= $n"
-echo "MNCount= $(python -c 'import os; print "{0:>14,.0f}".format(float(os.environ["MNCount"]))')"
 Perc2=$(python -c 'import os; print "{:>13,.2f}".format((float(os.environ["n"]) / float(os.environ["MNCount"]) * 100))')
 
 echo "Percentage of total GUAP Voting Power           : $Perc2%"
