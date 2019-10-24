e6a86cb654603efb814a4a843464287f2ccb4944
Jason Lamb <jlamb@lambcorp.com>
2019-10-24 15:17:06 -0400

    Update ChkSavGUAPholdingsv1.sh

--- ChkSavGUAPholdingsv1.sh
+++ ChkSavGUAPholdingsv1.sh
@@ -116,7 +116,7 @@ echo ""
 echo "-----------------------------------------------------------------"
 GUAPearned=$(python -c 'import os; print "{0:>5,.0f}".format((float(os.environ["MN_Total"]) - float(os.environ["LastGuapTotal"])))')
 #TimeElapsed=$((d_epoch-LastGuapTime))
-TimeElapsed=$(datediff $d_epoch $LastGuapTime -f '&dd &Hh &Ss')
+TimeElapsed=$(dateutils.ddiff $d_epoch $LastGuapTime -f '&dd &Hh &Ss')
 #TimeElapsed_s=$(date -d  @$TimeElapsed +'%S')
 #echo "  GUAP earned since last check @ $(date -d  @$LastGuapTime +'%m/%d %I:%M%P')  : $GUAPearned in last $(date -d  @$TimeElapsed +'%M%S') min"
 echo "  GUAP earned since last check @ $(date -d  @$LastGuapTime +'%m/%d %I:%M%P')  : $GUAPearned in last $TimeElapsed"
